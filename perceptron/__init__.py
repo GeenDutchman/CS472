028b381cdf8e824fbf02feed75a9c439463bb1ed
Drake Foltz <GeenDutchman@byu.net>
2020-01-27 21:17:57 -0700

    Added arff2

--- perceptron/__init__.py
+++ perceptron/__init__.py
@@ -30,7 +30,7 @@ print("Final Weights =", PClass.get_weights())
 
 print("--------------arf2------------------------------")
 
-mat = Arff("../datasets/data_banknote_authentication.arff", label_count=1)
+mat = Arff("../data/perceptron/evaluation/data_banknote_authentication.arff", label_count=1)
 np_mat = mat.data
 data = mat[:, :-1]
 labels = mat[:, -1].reshape(-1, 1)
@@ -40,4 +40,4 @@ P2Class = PerceptronClassifier(lr=0.1, shuffle=False, deterministic=10)
 P2Class.fit(data, labels)
 Accuracy = P2Class.score(data, labels)
 print("Accuray = [{:.2f}]".format(Accuracy))
-print("Final Weights =", P2Class.get_weights()
+print("Final Weights =", P2Class.get_weights())
