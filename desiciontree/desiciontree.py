7b4e1bf2df97a8bc7b0075b79a03eeacab487419
Drake Foltz <GeenDutchman@byu.net>
2020-03-03 00:07:41 -0700

    I don't quite like how _fit works

--- desiciontree/desiciontree.py
+++ desiciontree/desiciontree.py
@@ -29,23 +29,26 @@ class DTClassifier(BaseEstimator,ClassifierMixin):
         return label_results[0][highest_index]
 
     def _fit(self, X, y, branch, indexes):
-        best_branch = (0, None)
-        out_of, data_results, label_results = self._count_unique(X, y)
-        entropy = self._entropy(out_of, label_results)
+        best_branch = (0, None, 0)
+        out_of, data_results = self._count_unique(X)
+        label_size, label_results = self._count_unique(y)
+        entropy = self._entropy(label_size, label_results)
 
         for i in indexes:
             partitions = self._partition(X, y, i, data_results=data_results)
             sum = 0
             for part in partitions:
                 print(part, best_branch)
-                y_shape = np.shape(part[1])[0]
-                fraction = y_shape / out_of
-                sum = sum + self._entropy(y_shape, part[1]) * fraction
-                gain = entropy - sum
-                a_branch = Tree.Branch(self._most_common_class(part[1]), i, data_results[0])
-                if gain > best_branch[0]:
-                    best_branch = (gain, a_branch)
+                part_label_size, part_label_result = self._count_unique(part[1])
+                fraction = part_label_size / out_of
+                sum = sum + self._entropy(part_label_size, part_label_result) * fraction
+            gain = entropy - sum
+            a_branch = self.tree.makeBranch(self._most_common_class(label_results[0]), i, data_results[i][0])
+            if gain >= best_branch[0]:
+                best_branch = (gain, a_branch, i)
         print(best_branch)
+        self.tree.addBranch(best_branch[1], branch, best_branch[2])
+        indexes.remove(best_branch[2])
 
     def fit(self, X, y):
         """ Fit the data; Make the Desicion tree
@@ -88,22 +91,17 @@ class DTClassifier(BaseEstimator,ClassifierMixin):
         """
         return 0
 
-    def _count_unique(self, X, y):
+    def _count_unique(self, data):
         data_results = {}
-        for index in range(np.shape(X)[1]):
-            values, counts = np.unique(X[:,index], return_counts=True)
+        for index in range(np.shape(data)[1]):
+            values, counts = np.unique(data[:,index], return_counts=True)
             data_results[index] = (values, counts)
 
-        label_results = {}
-        for index in range(np.shape(y)[1]):
-            values, counts = np.unique(y[:,index], return_counts=True)
-            label_results[index] = (values, counts)
-
-        return np.shape(y)[0], data_results, label_results
+        return np.shape(data)[0], data_results
 
     def _partition(self, X, y, attribute_index, data_results=None):
         if data_results is None:
-            data_results = self._count_unique(X, y)[1]
+            data_results = self._count_unique(X)[1]
         partitions = []
         for label in data_results[attribute_index][0]:
             x_part = []
