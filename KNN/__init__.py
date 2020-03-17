19032a8e881b15465599bdd6b305094ef16b21ec
Drake Foltz <GeenDutchman@byu.net>
2020-03-16 22:39:54 -0600

    early test case

--- KNN/__init__.py
+++ KNN/__init__.py
@@ -1,1 +1,16 @@
+import numpy as np
+from KNN import KNNClassifier as my_knn
 
+def hwork():
+    print('------------hwork-------------')
+
+    data = np.array([[1,5],[0,8],[9,9],[10,10]])
+    labels = np.array([['a'],['b'],['b'],['a']])
+
+    classifier = my_knn()
+
+    classifier.fit(data, labels)
+    print(classifier(np.array([[2,6]])))
+
+
+hwork()
