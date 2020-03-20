61abdd4f67309c81e9eeca1078cc65180d29cfdb
Drake Foltz <GeenDutchman@byu.net>
2020-03-19 22:37:36 -0600

    basic stuff aaah

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -1,15 +1,17 @@
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
+from itertools import zip_longest
 
 class KNNClassifier(BaseEstimator,ClassifierMixin):
 
 
-    def __init__(self,labeltype=[],weight_type='inverse_distance',k=3): ## add parameters here
+    def __init__(self,labeltype=[],weight_type='inverse_distance',k=3,regression=False): ## add parameters here
         """
         Args:
             columntype for each column tells you if continues[real] or if nominal.
             weight_type: inverse_distance voting or if non distance weighting. Options = ["no_weight","inverse_distance"]
         """
+        self.regression = regression
         self.columntype = labeltype
         self.weight_type = weight_type
         self._default_diff = 1
@@ -19,7 +21,7 @@ class KNNClassifier(BaseEstimator,ClassifierMixin):
 
     def _distance(self, pointOne, pointTwo, diff_exponet=2, rooter=0.5):
         total = 0
-        for x, y in zip(pointOne, pointTwo):
+        for x, y, col_type in zip_longest(pointOne, pointTwo, self.columntype, fillvalue="real"):
             # if type(x) != type(y):
             #     total += self._default_diff ** diff_exponet
             # else:
@@ -46,14 +48,26 @@ class KNNClassifier(BaseEstimator,ClassifierMixin):
     def _analyze_neighbors(self, neighbors):
         inverse_distance = lambda dist: dist ** -2
         results = {}
-        for neighbor in neighbors:
-            this_neighbor = 1 if self.weight_type == "no_weight" else inverse_distance(neighbor[0])
-            if neighbor[1] in results:
-                results[neighbor[1]] += this_neighbor
-            else:
-                results[neighbor[1]] = this_neighbor
+        if not self.regression:
+            for neighbor in neighbors:
+                this_neighbor = 1 if self.weight_type == "no_weight" else inverse_distance(neighbor[0])
+                if neighbor[1] in results:
+                    results[neighbor[1]] += this_neighbor
+                else:
+                    results[neighbor[1]] = this_neighbor
 
-        return max(results, key=lambda k: results[k])
+            return max(results, key=lambda k: results[k])
+        else:
+            inverses = []
+            numerator = np.zeros(np.shape(neighbors[0][2]))
+            for neighbor in neighbors:
+                if self.weight_type == 'inverse_distance':
+                    inverses.append(inverse_distance(neighbor[0]))
+                else:
+                    inverses.append(1)
+                numerator = numerator + (inverses[-1] * neighbor[2])
+
+            return numerator / np.sum(inverses)
 
 
     def predict(self,data):
@@ -70,14 +84,14 @@ class KNNClassifier(BaseEstimator,ClassifierMixin):
         for predict_index in range(predict_instances):
             neighbors = [] # dist, label
             for stored_index in range(stored_instances):
-                neighbors.append((self._distance(self.data[stored_index], data[predict_index]), self.data_labels[stored_index][0]))
-            neighbors = sorted(neighbors)[:self.k]
+                neighbors.append((self._distance(self.data[stored_index], data[predict_index]), self.data_labels[stored_index][0], data[predict_index]))
+            neighbors = sorted(neighbors, key=lambda x: x[0])[:self.k]
             results.append(self._analyze_neighbors(neighbors))   
         
         return results, np.shape(results)
 
     #Returns the Mean score given input data and labels
-    def score(self, X, y, predict_results=None):
+    def score(self, X, y, predict_results=None, style="accuracy"):
             """ Return accuracy of model on a given dataset. Must implement own score function.
             Args:
                     X (array-like): A 2D numpy array with data, excluding targets
@@ -89,10 +103,18 @@ class KNNClassifier(BaseEstimator,ClassifierMixin):
             results = predict_results
             if results is None:
                 results = np.reshape(self.predict(X)[0], np.shape(y))
-            correct = 0
-            for scored, expected in zip(results, y):
-                if scored == expected:
-                    correct += 1
-            return 0 if len(results) == 0 else (correct / len(results)) * 100
+            if style=='accuracy':
+                correct = 0
+                for scored, expected in zip(results, y):
+                    if scored == expected:
+                        correct += 1
+                return 0 if len(results) == 0 else (correct / len(results)) * 100.0
+            if style=='mse':
+                summer = 0
+                count = 0
+                for scored, expected in zip(results, y):
+                    summer = summer + ((scored - expected) ** 2)
+                    count = count + 1
+                return summer / count
 
 
--- KNN/__init__.py
+++ KNN/__init__.py
@@ -1,6 +1,8 @@
 import numpy as np
 from KNN import KNNClassifier #as my_knn
 
+from sklearn.preprocessing import Normalizer
+
 from arff import Arff
 
 def hwork():
@@ -29,7 +31,7 @@ def seismic():
 
     classifier.fit(data, labels)
     score = classifier.score(data2, labels2)
-    print("Accuracy=[{:.2f}]".format(score))
+    print("Accuracy = [{:.2f}]".format(score))
 
 def evaluation():
     print("--------------evaluation-----------------")
@@ -62,15 +64,83 @@ def magic_telescope():
     data2 = mat2.data[:,0:-1]
     labels2 = mat2.data[:,-1].reshape(-1, 1)
 
-    
+    transformer = Normalizer()
+    dataN = transformer.transform(data)
+    data2N = transformer.transform(data2)    
 
-    classifier = KNNClassifier()
+    classifier = KNNClassifier(weight_type="no_weight")
 
     classifier.fit(data, labels)
     predicted = np.array(classifier.predict(data2)[0])
     np.savetxt('magic-telescope.csv', predicted, delimiter=',')
     score = classifier.score(data2, labels2, predict_results=predicted)
     print("Accuracy=[{:.2f}]".format(score))
+    classifier.fit(dataN, labels)
+    predicted = np.array(classifier.predict(data2N)[0])
+    np.savetxt('magic-telescopeN.csv', predicted, delimiter=',')
+    score = classifier.score(data2N, labels2, predict_results=predicted)
+    print("Accuracy N=[{:.2f}]".format(score))
+
+    accuracies = []
+    for k in range(1,17,2):
+        classifier = KNNClassifier(weight_type="no_weight", k=k)
+        accuracies.append(classifier.fit(dataN, labels).score(data2N, labels2))
+    np.savetxt('magic-telescopeAccuracies.csv', np.array(accuracies), delimiter=',')
+
+
+def housingNW():
+    print('-----------housing no weight-----------')
+    mat = Arff('./data/housing_train.arff', label_count=1)
+    mat2 = Arff('./data/housing_test.arff', label_count=1)
+
+    data = mat.data[:,0:-1]
+    labels = mat.data[:,-1].reshape(-1, 1)
+
+    data2 = mat2.data[:,0:-1]
+    labels2 = mat2.data[:,-1].reshape(-1, 1)
+
+    transformer = Normalizer()
+    dataN = transformer.transform(data)
+    data2N = transformer.transform(data2)
+
+    classifier = KNNClassifier(regression=True, weight_type="no_weight")
+
+    mse = classifier.fit(dataN, labels).score(data2N, labels2, style="mse")
+    print("MSE:", mse)
+
+    mse_scores = []
+    for k in range(1,17,2):
+        classifier = KNNClassifier(weight_type="no_weight", regression=True, k=k)
+        mse_scores.append(classifier.fit(dataN, labels).score(data2N, labels2, style='mse'))
+    np.savetxt('housing_mseNW.csv', np.array(mse_scores), delimiter=',')
+
+def housingWW():
+    print('-----------housing weighted-----------')
+    mat = Arff('./data/housing_train.arff', label_count=1)
+    mat2 = Arff('./data/housing_test.arff', label_count=1)
+
+    data = mat.data[:,0:-1]
+    labels = mat.data[:,-1].reshape(-1, 1)
+
+    data2 = mat2.data[:,0:-1]
+    labels2 = mat2.data[:,-1].reshape(-1, 1)
+
+    transformer = Normalizer()
+    dataN = transformer.transform(data)
+    data2N = transformer.transform(data2)
+
+    classifier = KNNClassifier(regression=True, weight_type="inverse_distance", labeltype=mat.attr_types)
+
+    mse = classifier.fit(dataN, labels).score(data2N, labels2, style="mse")
+    print("MSE:", mse)
+
+    mse_scores = []
+    for k in range(1,17,2):
+        classifier = KNNClassifier(weight_type="inverse_distance", regression=True, k=k)
+        mse_scores.append(classifier.fit(dataN, labels).score(data2N, labels2, style='mse'))
+    np.savetxt('housing_mseWW.csv', np.array(mse_scores), delimiter=',')
+
+
 
 
 hwork()
